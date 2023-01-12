159867820d097e1db60555046d370cdb66bd8ba4
hila@netcompany.com <hila@netcompany.com>
2023-01-12 14:09:22 +0100

    core logic implemented. mock models included

---
+++ MockModel/MockEdge.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+
+namespace EIT.MockModel
+{
+    public class MockEdge
+    {
+        public int Id { get; set; }
+        public int Source { get; set; }
+        public int Destination { get; set; }
+        public int Segments { get; set; }
+        public MockEdge(int id, int source, int destination , int segments) {
+            Id = id;
+            Source = source;
+            Destination = destination;
+            Segments = segments;
+        }
+        public static List<MockEdge> GetMockEdges() {
+            var edges = new List<MockEdge>
+            {
+                new MockEdge(1, 1, 7, 5),
+                new MockEdge(2, 7, 1, 5),
+                new MockEdge(3, 2, 16, 3 ),
+                new MockEdge(4, 16, 2, 3),
+                new MockEdge(5, 16, 15, 4),
+                new MockEdge(6, 15, 16, 4),
+                new MockEdge(7, 15, 14, 4),
+                new MockEdge(8, 14, 15, 4),
+                new MockEdge(9, 15, 29, 11),
+                new MockEdge(10, 29, 15, 11),
+                new MockEdge(11, 14, 29, 9),
+                new MockEdge(12, 29, 14, 9),
+                new MockEdge(13, 7, 17, 10),
+                new MockEdge(14, 17, 7, 10),
+                new MockEdge(15, 17, 30, 9),
+                new MockEdge(16, 30, 17, 9),
+                new MockEdge(17, 29, 30, 3),
+                new MockEdge(18, 30, 29, 3  ),
+                new MockEdge(19, 30, 26, 8),
+                new MockEdge(20, 26, 30, 8),
+                new MockEdge(21, 26, 24, 3),
+                new MockEdge(22, 24, 26, 3),
+                new MockEdge( 23, 24, 11, 8),
+                new MockEdge(24, 11, 24, 8),
+                new MockEdge(25, 11, 25, 8),
+                new MockEdge( 26, 25, 11, 8),
+                new MockEdge(27, 11, 10, 4),
+                new MockEdge(28, 10, 11, 4),
+                new MockEdge(29, 10, 32, 4),
+                new MockEdge(30, 32, 10, 4 ),
+                new MockEdge(31, 32, 3, 5),
+                new MockEdge(32, 3, 32, 5),
+                new MockEdge(33, 3, 31, 3),
+                new MockEdge(34, 31, 3, 3),
+                new MockEdge(35, 31, 1, 3),
+                new MockEdge(36, 1, 31, 3),
+            };
+            return edges; 
+        }
+    }
+}
--- RoutePlanner/RoutePlanner.cs
+++ RoutePlanner/RoutePlanner.cs
@@ -1,5 +1,12 @@
 ﻿using EIT.Interfaces;
+using EIT.MockModel;
 using EIT.Model;
+using System.Collections.Generic;
+using Dijkstra.NET.Graph;
+using Dijkstra.NET.ShortestPath;
+using System;
+using System.Linq;
+using Dijkstra.NET.Graph.Simple;
 
 namespace EIT.RoutePlanner
 {
@@ -17,15 +24,19 @@ namespace EIT.RoutePlanner
         public RouteResult GetRoute(int from, int to, int weight)
         {
             // Load topology
-            // var topology = LoadTopology()
-
-            // for each edge in edge
-            // numofsegment = edge[Segments]
-            // price = getPricePerSegment()
-            // cost = number of segment * price persegment
-            // UpdateTopology(edge, cost)
+            var topology = LoadTopology(weight);
 
             // if we have both source city and destination city
+            var ourAvailableCities = MockCity.GetAvailableId();
+            if(ourAvailableCities.Contains(from) && ourAvailableCities.Contains(to)) 
+            {
+                ShortestPathResult result = topology.Dijkstra((uint)from, (uint)to); //result contains the shortest path
+                var path = result.GetPath();
+                foreach (var item in path)
+                {
+                    Console.WriteLine(item);
+                }
+            }
                 // Compute Route
                 // Calculate cost, time
                 // Return from, to, cost, time, cost to competitors
@@ -54,12 +65,28 @@ namespace EIT.RoutePlanner
             // var path = result.GetPath();
         }
 
-        private void LoadTopology()
+        private Graph<int, string> LoadTopology(int weight)
         {
             // var cities = db.getCities(); //(Id, Name, Available)
+            List<MockCity> mockCities = MockCity.GetMockCities();
             // cities = cities.Where(x => x.Available);
 
             // var edges = db.getEdges(); //(Id, Source, Destination, Segments)
+            List<MockEdge> mockEdges = MockEdge.GetMockEdges();
+
+            var graph = new Graph<int, string>();
+
+            foreach (var city in mockCities)
+            {
+                graph.AddNode(city.Id);
+            }
+
+            foreach (var edge in mockEdges)
+            {
+                graph.Connect((uint)edge.Source, (uint)edge.Destination, edge.Segments * GetPrice(weight), "dummy string"); //First node has key equal 1
+            }
+
+            return graph;
         }
 
         private void UpdateTopology()
@@ -67,11 +94,50 @@ namespace EIT.RoutePlanner
             // graph.Add(edge[Source], edge[Destination], cost)
         }
 
-        private void GetPrice(int weight)
+        private int GetPrice(int weight)
         {
             // load [WeightPrices](Id, Minimum, Maximum, Price)
+            var mockWeightPrices = MockWeightPrices.GetMockWeightPrices();
             // make if else statement for the current month
             // make nested if else statement for the current weight
+            DateTime dt = DateTime.Now;
+            var currentMonth = dt.Month; //integer between 1 and 12.
+            if(currentMonth >= 5 && currentMonth <= 10) //May-Oct
+            {
+                if(weight < 10) // kilogram
+                {
+                    var price = mockWeightPrices.Where(x => x.Id == 2).Single();
+                    return price.Price;
+                }
+                else if(weight < 50)
+                {
+                    var price = mockWeightPrices.Where(x => x.Id == 4).Single();
+                    return price.Price;
+                }
+                else
+                {
+                    var price = mockWeightPrices.Where(x => x.Id == 6).Single();
+                    return price.Price;
+                }
+            }
+            else //Nov-April
+            {
+                if (weight < 10) // kilogram
+                {
+                    var price = mockWeightPrices.Where(x => x.Id == 1).Single();
+                    return price.Price;
+                }
+                else if (weight < 50)
+                {
+                    var price = mockWeightPrices.Where(x => x.Id == 3).Single();
+                    return price.Price;
+                }
+                else
+                {
+                    var price = mockWeightPrices.Where(x => x.Id == 5).Single();
+                    return price.Price;
+                }
+            }
             // return the price
         }
     }
