5469971331634736c5bf92f13235aeb727684853
ssbo@netcompany.com <ssbo@netcompany.com>
2023-01-12 11:04:06 +0100

    Setup

--- ReactApp/vite.config.ts
+++ ReactApp/vite.config.ts
@@ -1,9 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
-import svgrPlugin from 'vite-plugin-svgr';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import viteTsconfigPaths from 'vite-tsconfig-paths'
+import svgrPlugin from 'vite-plugin-svgr'
+import dns from 'dns'
+
+dns.setDefaultResultOrder('verbatim')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
-});
+  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  build: {
+    outDir: 'build',
+    minify: false,
+    write: true,
+  },
+  server: {
+    hmr: {
+      protocol: 'ws',
+    },
+  },
+})
