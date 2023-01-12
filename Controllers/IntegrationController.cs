8f8b545d0137fb44bfa3095f7c0f6849cd585a5b
hila@netcompany.com <hila@netcompany.com>
2023-01-12 16:14:28 +0100

    integration controller dummy

---
+++ Controllers/IntegrationController.cs
@@ -0,0 +1,27 @@
+﻿using EIT.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace EIT.Controllers
+{
+    [ApiController]
+    [Route("[api]")]
+    public class IntegrationController : Controller
+    {
+
+        [HttpGet("/GetRoute", Name = nameof(GetRoute))]
+        public async Task<ActionResult<RouteIntegrationResponse>> GetRoute([FromBody]RouteIntegrationRequest routeIntegrationRequest)
+        {
+
+            //    return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+            return new RouteIntegrationResponse
+            {
+                CorrelationID = Guid.NewGuid().ToString(),
+                Cost = 100,
+                Time = 100
+            };
+        }
+    }
+}
